6928b3b8dbf03382a29dd76d0a6fffcf41b58df7
hehehe <hehehehe>
2016-04-08 21:52:33 +0100

    Initial commit

---
+++ Accomodation tour app/Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void Pavs(){
+        SceneManager.LoadScene("MyRoom");
+    }
+    void Courts()
+    {
+        SceneManager.LoadScene("Labs");
+    }
+
+
+    void Bray(){
+        SceneManager.LoadScene("Hill");
+    }
+
+}
---
+++ Accomodation tour app/Assets/Open.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+function Start () {
+    yield WaitForSeconds(20);
+    Application.LoadLevel("LincolnAccomodation");
+}
+
+function Update () {
+
+}
+
+function Pavillions () {
+    Application.LoadLevel("MyRoom");
+}
+function Courts () {
+    Application.LoadLevel("Labs");
+}
+
+function Brayford () {
+    Application.LoadLevel("Hill");
+}
+
+function ExitApplication () {
+    Application.Quit();
+}
